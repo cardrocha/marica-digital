954d436a4df3ac148eb74cf7c971b1015fef5fb9
cardrocha <cardrocharj@gmail.com>
2025-02-17 18:49:23 -0300

    feat: add logo and improve header navigation with links

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,15 +1,40 @@
+import logo from "@/public/marica.svg";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Header() {
-  return ( 
+  return (
     <header className="flex items-center justify-between text-white gap-8 py-4 px-8 bg-black">
-      <h1>Maricá Digital</h1>
+      <Link href="#" title="clique aqui" className="flex items-center gap-4">
+        <Image
+          className="rounded-md"
+          src={logo}
+          width={48}
+          height={48}
+          alt="Logo do Maricá Digital"
+        />
+        <h1 className="text-2xl">Maricá Digital</h1>
+      </Link>
       <nav>
         <ul className="flex gap-4">
-          <li>Home</li>
-          <li>Sobre</li>
-          <li>Entretenimento</li>
-          <li>Esportes</li>
-          <li>Contato</li>
-          <li>Cadastro</li>
+          <Link className="hover:underline" href="#" title="ir para Home">
+            <li>Home</li>
+          </Link>
+          <Link className="hover:underline" href="#" title="ir para Sobre">
+            <li>Sobre</li>
+          </Link>
+          <Link className="hover:underline" href="#" title="ir para Entretenimento">
+            <li>Entretenimento</li>
+          </Link>
+          <Link className="hover:underline" href="#" title="ir para Esportes">
+            <li>Esportes</li>
+          </Link>
+          <Link className="hover:underline" href="#" title="ir para Contatos">
+            <li>Contatos</li>
+          </Link>
+          <Link className="hover:underline" href="#" title="ir para Cadastro">
+            <li>Cadastro</li>
+          </Link>
         </ul>
       </nav>
     </header>
