6a263a84cc9afaa2e3ba3b54b863ee2fb5bea991
cardrocha <cardrocharj@gmail.com>
2025-02-17 18:19:16 -0300

    feat: enhance layout structure and add custom 404 page

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,10 +27,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
         <Header />
-        {children}
+        <main className="flex flex-1">{children}</main>
         <Footer />
       </body>
     </html>
---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return ( 
+    <main className="flex flex-1 justify-center items-center min-h-screen">
+      <h1 className="text-3xl font-semibold">404 - Página não encontrada</h1>  
+    </main>
+  );
+}
+
